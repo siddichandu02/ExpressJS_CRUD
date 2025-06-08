952503e522926b9c27a85352815ba119c5fd3e65
siddichandu02 <siddichandu02@gmail.com>
2025-06-08 14:09:24 -0500

    added logging using winston and morgan

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 import "dotenv/config";
 // File: index.js
 import express from "express";
-
+import logger from "./logger.js";
+import morgan from "morgan";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = [];
 let nextid = 1;
